d4e3e69cda611e846a82acf9fa254296b43fc1c6
FrancesDiaz <115190000+francesvdiaz@users.noreply.github.com>
2023-02-15 19:17:55 -0800

    Add updateBalance function to keep track of money in bank account after each refresh

--- js/scripts.js
+++ js/scripts.js
@@ -16,10 +16,11 @@ BankAccount.prototype = {
   }
 };
 
+
 function bankTransaction(transactionType, amount, bankAccount) {
   if (transactionType === "deposit") {
     bankAccount.deposit(amount);
-  } else if (transactionType==="withdrawal") {
+  } else if (transactionType==="withdraw") {
     bankAccount.withdraw(amount);
   } else {
     console.log("invalid, try again.")
@@ -27,14 +28,21 @@ function bankTransaction(transactionType, amount, bankAccount) {
   return bankAccount.getBalance();
 }
 
+let myBankAccount = new BankAccount();
+
+function updateBalance() {
+  const balanceElement = document.getElementById("balance");
+  balanceElement.textContent = "$" + myBankAccount.getBalance().toFixed(2);
+}
+
 window.addEventListener("load", function() {
   document.getElementById("depositButton").addEventListener("click", handleDepositSubmission);
   document.getElementById("withdrawButton").addEventListener("click", handleWithdrawSubmission);
+  updateBalance();
 });
 
 function handleDepositSubmission(event) {
   event.preventDefault();
-  let myBankAccount = new BankAccount();
   const depositValue = parseFloat(document.getElementById("deposit").value);
   
   if (isNaN(depositValue)) {
@@ -43,6 +51,7 @@ function handleDepositSubmission(event) {
   }
   
   myBankAccount.deposit(depositValue);
+  updateBalance();
 
   const balanceElement = document.getElementById("balance");
   balanceElement.textContent = " $" + myBankAccount.getBalance().toFixed(2);
@@ -50,16 +59,30 @@ function handleDepositSubmission(event) {
 
 function handleWithdrawSubmission(event) {
   event.preventDefault();
-  let myBankAccount = new BankAccount();
   const withdrawalValue = parseFloat(document.getElementById("withdrawal").value);
   
+  if (myBankAccount.PIN !== PIN) {
+    console.log("PIN is incorrect, try again.");
+    return;
+  }
+
   if (isNaN(withdrawalValue)) {
     console.log("invalid withdrawal value, try again.");
     return;
   }
   
   myBankAccount.withdraw(withdrawalValue);
+  updateBalance();
 
   const balanceElement = document.getElementById("balance");
   balanceElement.textContent = " $" + myBankAccount.getBalance().toFixed(2);
 };
+
+
+//Alt. for BankAccount Constructor
+
+function BankAccount(accountName, PIN) {
+  this.accountName = accountName;
+  this.PIN = PIN;
+  this.balance = 0;
+}
